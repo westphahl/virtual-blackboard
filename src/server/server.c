4f734ce17f7864a081dafb33e67cc67fe7125e91
Simon Westphahl <westphahl@gmail.com>
2010-10-25 13:45:29 +0200

    Adds basic fork skeleton.

--- src/server/server.c
+++ src/server/server.c
@@ -12,11 +12,15 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include "../commons.h"
 
 int main(int argc, char **argv) {
-    unsigned int listen_port = 50000;
+    unsigned int listen_port = DEFAULT_PORT;
     int debug = 0;
     int opt;
+    pid_t l_pid;
+    pid_t a_pid;
 
     // Parse command line options
     while ((opt = getopt(argc, argv, "dp:")) != -1) {
@@ -26,11 +30,38 @@ int main(int argc, char **argv) {
                 debug = 1;
                 break;
             case 'p':
-                // TODO Add option for listen port
-                // listen_port = strtol(opt, NULL, 10);
+                // Use a user defined port
+                listen_port = strtol(optarg, NULL, 10);
+                if ((listen_port < PORT_RANGE_MIN) || (listen_port > PORT_RANGE_MAX)) {
+                    fprintf(stderr,
+                        "Invalid port range: must be between %i and %i.\n" \
+                        "Falling back to default %i\n",
+                        PORT_RANGE_MIN, PORT_RANGE_MAX, DEFAULT_PORT);
+                    listen_port = DEFAULT_PORT;
+                }
                 break;
         }
     }
-    
+
+    // Fork Logger
+    l_pid = fork();
+    if (l_pid == 0) {
+        // Logger: excel()
+    } else if (l_pid < 0) {
+        perror("fork logger");
+        exit(EXIT_FAILURE);
+    } else {
+        // Fork Archiver
+        a_pid = fork();
+        if (a_pid == 0) {
+            // Archiver: excel()
+        } else if (a_pid < 0) {
+            perror("fork archiver");
+            exit(EXIT_FAILURE);
+        } else {
+            // Server goes here
+        }
+    }
+
     exit(EXIT_SUCCESS);
 }
