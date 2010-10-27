d803a755dd42412df8b5207c8758a94dc85f6818
Michael Wydler <michael.wydler@googlemail.com>
2010-10-27 18:44:09 +0200

    Add message-structure for logger

--- src/server/mq.h
+++ src/server/mq.h
@@ -1,6 +1,13 @@
 #ifndef MQ_H
 #define MQ_H
 
+struct logmessage {
+    int level;
+    time_t time;
+    char *message;
+};
+typedef struct logmessage LOGMESSAGE;
+
 int create_mq(key_t key);
 int get_mq(key_t key);
 void delete_mq(int mq_id);
